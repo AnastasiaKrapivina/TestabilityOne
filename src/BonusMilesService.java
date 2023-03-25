973b6f23ce0f677a9c15bf20f22a5dea8bb5a2a1
Anastasia Krapivina <krapivina934@gmail.com>
2023-03-25 14:04:57 +0700

    Cheange one

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+
+        int result;
+        int mile = 20;
+        result = price / mile;
+
+        return result;
+
+    }
+}
